4973a704c2f8f5155ac5f37d1b959518447efac1
simonostendorf <github@simon-ostendorf.de>
2022-02-14 09:10:24 +0100

    rename update command

--- src/chat/commands/uptime.ts
+++ src/chat/commands/uptime.ts
@@ -2,9 +2,9 @@ import { TwitchPrivateMessage } from '@twurple/chat/lib/commands/TwitchPrivateMe
 import { getUptime, millisToString } from '../../utils/time';
 import { getChatClient } from '../chat';
 
-export const name: string = '!uptime';
+export const name: string = '!botuptime';
 
-// !uptime
+// !botuptime
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function handle(channelname: string, username: string, message: string, msg: TwitchPrivateMessage) {
